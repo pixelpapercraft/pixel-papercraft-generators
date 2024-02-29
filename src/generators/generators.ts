4ccba8d3a6705687cbf338a7996e04ff772f5e1a
Pixel Papercraft <pixelpapercraft@gmail.com>
2024-03-01 09:28:49 +1100

    Tweak generator imports

--- src/generators/generators.ts
+++ src/generators/generators.ts
@@ -1,85 +1,85 @@
 import { type GeneratorDef } from "@/builder/modules/generatorDef";
-import * as ExampleGenerator from "@/generators/example/exampleGenerator";
-import * as AmogusBendableGenerator from "@/generators/amogusBendable/amogusBendableGenerator";
-import * as DalekModDalekGenerator from "@/generators/dalekModDalek/dalekModDalekGenerator";
-import * as MinecraftActionFigureGenerator from "@/generators/minecraftActionFigure/minecraftActionFigureGenerator";
-import * as MinecraftAllayCharacterGenerator from "@/generators/minecraftAllayCharacter/minecraftAllayCharacterGenerator";
-import * as MinecraftAxolotlCharacterGenerator from "@/generators/minecraftAxolotlCharacter/minecraftAxolotlCharacterGenerator";
-import * as MinecraftBeeCharacterGenerator from "@/generators/minecraftBeeCharacter/minecraftBeeCharacterGenerator";
-import * as MinecraftCapeAndElytraGenerator from "@/generators/minecraftCapeAndElytra/minecraftCapeAndElytraGenerator";
-import * as MinecraftCatGenerator from "@/generators/minecraftCat/minecraftCatGenerator";
-import * as MinecraftCharacterGenerator from "@/generators/minecraftCharacter/minecraftCharacterGenerator";
-import * as MinecraftCharacterHeadsGenerator from "@/generators/minecraftCharacterHeads/minecraftCharacterHeadsGenerator";
-import * as MinecraftCharacterMiniGenerator from "@/generators/minecraftCharacterMini/minecraftCharacterMiniGenerator";
-import * as MinecraftCowCharacterGenerator from "@/generators/minecraftCowCharacter/minecraftCowCharacterGenerator";
-import * as MinecraftCreeperGenerator from "@/generators/minecraftCreeper/minecraftCreeperGenerator";
-import * as MinecraftCreeperCharacterGenerator from "@/generators/minecraftCreeperCharacter/minecraftCreeperCharacterGenerator";
-import * as MinecraftEndermanGenerator from "@/generators/minecraftEnderman/minecraftEndermanGenerator";
-import * as MinecraftEndermanCharacterGenerator from "@/generators/minecraftEndermanCharacter/minecraftEndermanCharacterGenerator";
-import * as MinecraftGolemGenerator from "@/generators/minecraftGolem/minecraftGolemGenerator";
-import * as MinecraftGolemCharacterGenerator from "@/generators/minecraftGolemCharacter/minecraftGolemCharacterGenerator";
-import * as MinecraftHorseGenerator from "@/generators/minecraftHorse/minecraftHorseGenerator";
+import { generator as exampleGenerator } from "@/generators/example/exampleGenerator";
+import { generator as amogusBendableGenerator } from "@/generators/amogusBendable/amogusBendableGenerator";
+import { generator as dalekModDalekGenerator } from "@/generators/dalekModDalek/dalekModDalekGenerator";
+import { generator as minecraftActionFigureGenerator } from "@/generators/minecraftActionFigure/minecraftActionFigureGenerator";
+import { generator as minecraftAllayCharacterGenerator } from "@/generators/minecraftAllayCharacter/minecraftAllayCharacterGenerator";
+import { generator as minecraftAxolotlCharacterGenerator } from "@/generators/minecraftAxolotlCharacter/minecraftAxolotlCharacterGenerator";
+import { generator as minecraftBeeCharacterGenerator } from "@/generators/minecraftBeeCharacter/minecraftBeeCharacterGenerator";
+import { generator as minecraftCapeAndElytraGenerator } from "@/generators/minecraftCapeAndElytra/minecraftCapeAndElytraGenerator";
+import { generator as minecraftCatGenerator } from "@/generators/minecraftCat/minecraftCatGenerator";
+import { generator as minecraftCharacterGenerator } from "@/generators/minecraftCharacter/minecraftCharacterGenerator";
+import { generator as minecraftCharacterHeadsGenerator } from "@/generators/minecraftCharacterHeads/minecraftCharacterHeadsGenerator";
+import { generator as minecraftCharacterMiniGenerator } from "@/generators/minecraftCharacterMini/minecraftCharacterMiniGenerator";
+import { generator as minecraftCowCharacterGenerator } from "@/generators/minecraftCowCharacter/minecraftCowCharacterGenerator";
+import { generator as minecraftCreeperGenerator } from "@/generators/minecraftCreeper/minecraftCreeperGenerator";
+import { generator as minecraftCreeperCharacterGenerator } from "@/generators/minecraftCreeperCharacter/minecraftCreeperCharacterGenerator";
+import { generator as minecraftEndermanGenerator } from "@/generators/minecraftEnderman/minecraftEndermanGenerator";
+import { generator as minecraftEndermanCharacterGenerator } from "@/generators/minecraftEndermanCharacter/minecraftEndermanCharacterGenerator";
+import { generator as minecraftGolemGenerator } from "@/generators/minecraftGolem/minecraftGolemGenerator";
+import { generator as minecraftGolemCharacterGenerator } from "@/generators/minecraftGolemCharacter/minecraftGolemCharacterGenerator";
+import { generator as minecraftHorseGenerator } from "@/generators/minecraftHorse/minecraftHorseGenerator";
 
 const isDevEnvironment: boolean = process.env.NODE_ENV === "development";
 
 export const character: GeneratorDef[] = [
-  MinecraftCharacterGenerator.generator,
-  MinecraftActionFigureGenerator.generator,
-  // MinecraftUltimateBendableGenerator.generator,
-  MinecraftCharacterMiniGenerator.generator,
+  minecraftCharacterGenerator,
+  minecraftActionFigureGenerator,
+  // minecraftUltimateBendableGenerator,
+  minecraftCharacterMiniGenerator,
 ];
 
 export const mobCharacter: GeneratorDef[] = [
-  MinecraftCreeperCharacterGenerator.generator,
-  // MinecraftCatCharacterGenerator.generator,
-  MinecraftCowCharacterGenerator.generator,
-  MinecraftEndermanCharacterGenerator.generator,
-  MinecraftGolemCharacterGenerator.generator,
-  // MinecraftPigCharacterGenerator.generator,
-  // MinecraftSquidCharacterGenerator.generator,
-  // MinecraftVillagerCharacterGenerator.generator,
-  // MinecraftWolfCharacterGenerator.generator,
-  MinecraftAxolotlCharacterGenerator.generator,
-  MinecraftAllayCharacterGenerator.generator,
-  MinecraftBeeCharacterGenerator.generator,
+  minecraftCreeperCharacterGenerator,
+  // minecraftCatCharacterGenerator,
+  minecraftCowCharacterGenerator,
+  minecraftEndermanCharacterGenerator,
+  minecraftGolemCharacterGenerator,
+  // minecraftPigCharacterGenerator,
+  // minecraftSquidCharacterGenerator,
+  // minecraftVillagerCharacterGenerator,
+  // minecraftWolfCharacterGenerator,
+  minecraftAxolotlCharacterGenerator,
+  minecraftAllayCharacterGenerator,
+  minecraftBeeCharacterGenerator,
 ];
 
 export const mob: GeneratorDef[] = [
-  MinecraftCreeperGenerator.generator,
-  MinecraftEndermanGenerator.generator,
-  MinecraftGolemGenerator.generator,
-  MinecraftHorseGenerator.generator,
-  // MinecraftPigGenerator.generator,
-  MinecraftCatGenerator.generator,
-  // MinecraftVillagerGenerator.generator,
+  minecraftCreeperGenerator,
+  minecraftEndermanGenerator,
+  minecraftGolemGenerator,
+  minecraftHorseGenerator,
+  // minecraftPigGenerator,
+  minecraftCatGenerator,
+  // minecraftVillagerGenerator,
 ];
 
 // Blocks, Items and Accessories
 export const utility: GeneratorDef[] = [
-  // MinecraftBlockGenerator.generator,
-  // MinecraftItemGenerator.generator,
-  MinecraftCapeAndElytraGenerator.generator,
-  MinecraftCharacterHeadsGenerator.generator,
+  // minecraftBlockGenerator,
+  // minecraftItemGenerator,
+  minecraftCapeAndElytraGenerator,
+  minecraftCharacterHeadsGenerator,
 ];
 
 export const mod: GeneratorDef[] = [
-  // MinecraftMutantCharacterGenerator.generator,
-  DalekModDalekGenerator.generator,
+  // minecraftMutantCharacterGenerator,
+  dalekModDalekGenerator,
 ];
 
-export const other: GeneratorDef[] = [AmogusBendableGenerator.generator];
+export const other: GeneratorDef[] = [amogusBendableGenerator];
 
 // Incomplete and in development
 export const dev: GeneratorDef[] = isDevEnvironment
   ? [
-      //  MinecraftWitherGenerator.generator
+      //  minecraftWitherGenerator
     ]
   : [];
 
 export const test: GeneratorDef[] = isDevEnvironment
   ? [
-      ExampleGenerator.generator,
-      // TestingGenerator.generator
+      exampleGenerator,
+      // testingGenerator
     ]
   : [];
 
