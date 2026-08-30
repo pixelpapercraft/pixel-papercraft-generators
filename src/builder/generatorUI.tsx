eb193b2bf8835a9f8bcac0ecde356b9b82a7d5c4
Pixel Papercraft <pixelpapercraft@gmail.com>
2026-08-30 17:50:03 +1000

    Move BooleanControl into GeneratorUI

--- src/builder/generatorUI.tsx
+++ src/builder/generatorUI.tsx
@@ -7,7 +7,6 @@ import { History } from "@genroot/builder/ui/history";
 import { MediaHero } from "@genroot/builder/ui/mediaHero";
 import { AtlasControl } from "@genroot/builder/ui/controls/atlasControl";
 import { TextureControl } from "@genroot/builder/ui/controls/textureControl";
-import { BooleanControl as BooleanControlV1 } from "@genroot/builder/ui/controls/booleanControl";
 import { ButtonControl as ButtonControlV1 } from "@genroot/builder/ui/controls/buttonControl";
 import { RangeControl as RangeControlV1 } from "@genroot/builder/ui/controls/rangeControl";
 import { LoadedTextureControl } from "./loadedTextureControl";
@@ -31,8 +30,36 @@ export function BooleanControl({
   checked,
   onCheckedChange,
 }: BooleanControlProps): JSX.Element {
+  const inputId = React.useId();
+  const onInputChange = () => onCheckedChange(!checked);
+
   return (
-    <BooleanControlV1 id={label} checked={checked} onChange={onCheckedChange} />
+    <div className="mb-4">
+      <div className="flex flex-col">
+        <label
+          className="mt-3 inline-flex items-center cursor-pointer"
+          htmlFor={inputId}
+        >
+          <span className="relative">
+            <span className="block w-10 h-6 bg-gray-300 rounded-full shadow-inner" />
+            <span
+              className={`absolute block w-4 h-4 mt-1 ml-1 rounded-full inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-50 ease-in-out ${
+                checked ? "bg-blue-500 transform translate-x-full" : "bg-white"
+              }`}
+            >
+              <input
+                id={inputId}
+                type="checkbox"
+                className="absolute opacity-0 w-0 h-0"
+                checked={checked}
+                onChange={onInputChange}
+              />
+            </span>
+          </span>
+          <span className="ml-3">{label}</span>
+        </label>
+      </div>
+    </div>
   );
 }
 
